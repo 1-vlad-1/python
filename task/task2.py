ed4eb1827f47450f9b120981a79ef422b0b695dc
1-vlad-1 <vlad01022002@mail.ru>
2023-03-18 23:46:17 +0300

    taskLecture2 made a function

--- task/task2.py
+++ task/task2.py
@@ -1,8 +1,11 @@
 import os
 
-for dirpath, dirnames, filenames in os.walk('C:/Program Files'):
-    for dirname in dirnames :
-        print("Catalog:", os.path.join(dirpath, dirname))
-    for filename in filenames :
-        print("File:", os.path.join(dirpath, filename))
-        print("File Size:", os.path.getsize(dirpath))
+def file_structure(path):
+    for dirpath, dirnames, filenames in os.walk(path):
+        for dirname in dirnames :
+            print("Catalog:", os.path.join(dirpath, dirname))
+        for filename in filenames :
+            print("File:", os.path.join(dirpath, filename))
+            print("File Size:", os.path.getsize(dirpath))
+
+file_structure('C:/Users/TEMP')
